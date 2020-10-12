d0c0010a3dd932d543a6f35af23fb0882545f84b
unknown <33037196+Aaerialys@users.noreply.github.com>
2020-10-11 23:04:20 -0400

    Updates

---
+++ precache-manifest.c5742becd792bfac9a84df7cd8b092a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635e5f78be01b0c7dc4187778b6e34ad",
+    "url": "/grid-simulator/index.html"
+  },
+  {
+    "revision": "f07824cde679c268c4d4",
+    "url": "/grid-simulator/static/css/main.2dd06d13.chunk.css"
+  },
+  {
+    "revision": "6fcb37ff623b5a1b3f73",
+    "url": "/grid-simulator/static/js/2.7618882c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grid-simulator/static/js/2.7618882c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07824cde679c268c4d4",
+    "url": "/grid-simulator/static/js/main.e1545335.chunk.js"
+  },
+  {
+    "revision": "4f1ab1c553a3abcb7ac3",
+    "url": "/grid-simulator/static/js/runtime-main.475f2a15.js"
+  },
+  {
+    "revision": "60a6c051c1563a067dd7166123a58698",
+    "url": "/grid-simulator/static/media/FORTE.60a6c051.TTF"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/grid-simulator/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/grid-simulator/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "3ef9183025d87f0407c8cbaa30fa7363",
+    "url": "/grid-simulator/static/media/logo.3ef91830.ico"
+  }
+]);
